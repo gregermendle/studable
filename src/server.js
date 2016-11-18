d75dc680537c5099380107f344a32afaf4be9d04
Greg <gjordan@umassd.edu>
2016-11-18 16:48:11 -0500

    build(Webpack): Add webpack config and middleware to server entry point. Move index.html to public f

--- src/server.js
+++ src/server.js
@@ -2,15 +2,36 @@ import express from 'express';
 import path from 'path';
 import config from './config/config';
 import winston from 'winston';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpack from 'webpack';
+import webpackConfig from './webpack.config';
 
 let app = express();
 let router = express.Router();
-const INDEX_PATH = path.join(__dirname, 'client', 'index.html');
+const INDEX_PATH = path.join(__dirname, 'public', 'index.html');
+const PUBLIC_PATH = path.join(__dirname, 'public');
 
+/*
+  Basic router serves index.html file with react root component
+*/
 router.get('/', function(req, res, next) {
   res.sendFile(INDEX_PATH);
 });
 
+/*
+  Webpack middleware configuartion
+*/
+let compiler = webpack(webpackConfig);
+app.use(webpackHotMiddleware(compiler));
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true, publicPath: webpackConfig.output.publicPath
+}));
+
+/*
+  Apply middleware to app and start server
+*/
+app.use('/', express.static(PUBLIC_PATH));
 app.use(router);
 let server = app.listen(config.port, function() {
   winston.log('info', 'Connection established on port: ' + config.port);
---
+++ src/webpack.config.js
@@ -0,0 +1,39 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const ENTRY = ['app.jsx', 'webpack-hot-middleware/client'];
+const CONTEXT_PATH = path.join(__dirname, 'client');
+const OUTPUT_PATH = path.join(__dirname, 'public');
+const OUTPUT_FILENAME = 'bundle.js';
+
+const webpackConfig = {
+  entry: ENTRY,
+  context: CONTEXT_PATH,
+  resolve: ['', '.js', '.jsx', '.css', '.less', '.es6'],
+  devtool: 'source-map',
+  output: {
+    path: OUTPUT_PATH,
+    filename: OUTPUT_FILENAME,
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel?cacheDirectory'
+      }, {
+        test: /\.less|\.css/,
+        exclude: /node_modules/,
+        loader: 'style!css!less'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
+
+export default webpackConfig;
